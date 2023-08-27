2f30036ca0c32080245baaf5137bbe737f3b89ef
Joao Victor <gmvictorr@outlook.com>
2023-08-26 23:42:00 -0300

    Adicionando arquivos modificados

--- src/Principal.java
+++ src/Principal.java
@@ -14,23 +14,22 @@ import javafx.stage.Stage;
  * @author jvmaiscedo
  */
 public class Principal extends Application {
-  
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+    Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
 }
